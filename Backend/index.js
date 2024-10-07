9ab35581ecf56f862ebe0f37502f1ff4dc79ff4c
Nicolasurazanp <nicolasurazanp@gmail.com>
2024-10-06 23:36:39 -0500

    "Connection Db"

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv';
 import conectarDB from "./config/db.js";
-// import usuarioRoutes from './routes/usuarioRoutes.js';
-// import proveedorRoutes from './routes/proveedorRoutes.js';
-// import pedidoRoutes from './routes/pedidoRoutes.js'; 
-// import proyectoRoutes from './routes/proyectoRoutes';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import proveedorRoutes from './routes/proveedorRoutes.js';
+import pedidoRoutes from './routes/pedidoRoutes.js'; 
+//import proyectoRoutes from './routes/proyectoRoutes';
 
 const app = express();
 
@@ -17,10 +17,10 @@ conectarDB();
 //Configurar CORS
 
 // // Routing
-// app.use("/api/usuarios", usuarioRoutes);
-// app.use("/api/proveedores", proveedorRoutes); 
-// app.use("/api/pedidos", pedidoRoutes); 
-// app.use('/api/proyectos', proyectoRoutes);
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proveedores", proveedorRoutes); 
+app.use("/api/pedidos", pedidoRoutes); 
+//app.use('/api/proyectos', proyectoRoutes);
 
 const PORT = process.env.PORT || 4000;
 
